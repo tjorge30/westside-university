c0493110db099fe604aa577a3700c43486160e09
tjorge30 <mrs.javalera@gmail.com>
2021-03-22 15:29:34 -0600

    complete

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
+import { HashRouter } from 'react-router-dom';
+import routes from './routes';
+import { Link } from 'react-router-dom'
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <nav className='nav'>
-          <div>WestSide University</div> 
-          <div className='link-wrap'>
-              <div className='links'>Home</div>
-              <div className='links'>About</div> 
-          </div>
-        </nav>
-      </div>
+      <HashRouter>
+        <div>
+          <nav className='nav'>
+            <div>WestSide University</div> 
+            <div className='link-wrap'>
+              <ul>
+                <Link className='links'><li>Home</li></Link>
+                <Link className='links'><li>About</li></Link> 
+              </ul>
+            </div>
+          </nav>
+          {routes}
+        </div>
+      </HashRouter>
     )
   }
 }
---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import ClassList from './components/ClassList/ClassList'
+
+export default (
+    <Switch>
+        <Route component={Home} exact path="/" />
+        <Route component={About} path="/about" />
+        <Route component={ClassList} path="/classlist/:class" />
+    </Switch>
+);
